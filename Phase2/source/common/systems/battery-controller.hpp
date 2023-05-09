b98c549a03b3dfbcb4fddcff9c4ce976b7147e29
MUSTAFA-Hamzawy <hamzawy2019579@gmail.com>
2023-05-10 00:09:10 +0300

    Battery System

---
+++ Phase2/source/common/systems/battery-controller.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "../ecs/world.hpp"
+#include "../components/movement.hpp"
+#include "../components/battery_bar.hpp"
+#include "../ecs/entity.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
+#include <glm/trigonometric.hpp>
+#include <glm/gtx/fast_trigonometry.hpp>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+namespace our
+{
+
+    class BatterySystem
+    {
+        const int MAX_COINS_TO_WIN = 15;
+        
+    public:
+
+        void update(World *world, string action)
+        {
+            const unordered_set<Entity *> entities = world->getEntities();
+            for (auto entity : entities)
+            {
+                if (entity->name == action)
+                {
+                    world->markForRemoval(entity);
+                    world->deleteMarkedEntities();
+
+                }
+            }
+ 
+        }
+
+        void update_battery(World *world, int coins_counter){
+            string action;
+            const int LEVEL_AMOUNT = 3;
+            switch (coins_counter)
+            {
+            case LEVEL_AMOUNT:
+                action = "bar-1";
+                break;
+            case 2 * LEVEL_AMOUNT:
+                action = "bar-2";
+                break;
+            case 3 * LEVEL_AMOUNT:
+                action = "bar-3";
+                break;
+            case 4 * LEVEL_AMOUNT:
+                action = "bar-4";
+                break;
+            case 5 * LEVEL_AMOUNT:
+                action = "bar-5";
+                break;
+            default:
+                return;
+                break;
+            }
+            this->update(world, action);
+        }
+    };
+
+}
