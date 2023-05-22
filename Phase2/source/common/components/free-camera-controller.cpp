f9b07e04ad2933b86d8a762c6c09857a78544355
AdhamAliAbdelAal <adhamali20012015@gmail.com>
2023-05-22 02:38:38 +0200

    Create dll Files Every Build

--- Phase2/source/common/components/free-camera-controller.cpp
+++ Phase2/source/common/components/free-camera-controller.cpp
@@ -6,8 +6,6 @@ namespace our {
     // Reads sensitivities & speedupFactor from the given json object
     void FreeCameraControllerComponent::deserialize(const nlohmann::json& data){
         if(!data.is_object()) return;
-        rotationSensitivity = data.value("rotationSensitivity", rotationSensitivity);
-        fovSensitivity = data.value("fovSensitivity", fovSensitivity);
         positionSensitivity = data.value("positionSensitivity", positionSensitivity);
         speedupFactor = data.value("speedupFactor", speedupFactor);
     }
--- Phase2/source/common/components/free-camera-controller.hpp
+++ Phase2/source/common/components/free-camera-controller.hpp
@@ -13,9 +13,6 @@ namespace our {
     // For a more simple example of how to use the ECS framework, see "movement.hpp"
     class FreeCameraControllerComponent : public Component {
     public:
-        // The senstivity paramter defined sensitive the camera rotation & fov is to the mouse moves and wheel scrolling
-        float rotationSensitivity = 0.01f; // The angle change per pixel of mouse movement
-        float fovSensitivity = 0.3f; // The fov angle change per unit of mouse wheel scrolling
         glm::vec3 positionSensitivity = {10.0f, 10.0f, 10.0f}; // The unity per second of camera movement if WASD is pressed
         float speedupFactor = 5.0f; // A multiplier for the positionSensitivity if "Left Shift" is held.
         float velocity = 10.0f; // The velocity of the jump
